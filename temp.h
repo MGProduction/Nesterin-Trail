d3468e0158541fb372a97f6ae90970d0721c9e25
Marco Giorgini <marco.giorgini@gmail.com>
2024-01-24 22:25:27 +0100

    scoring system added

--- temp.h
+++ temp.h
@@ -1,33 +1,33 @@
-// shortdict = 0.86% [0 - 306 bytes]
-// advnames = 1.47% [306 - 523 bytes]
-// advdesc = 23.31% [829 - 8317 bytes]
-// msgs = 24.35% [9146 - 8688 bytes]
-// msgs2 = 23.92% [17834 - 8533 bytes]
-// verbs = 1.41% [26367 - 503 bytes]
-// objs = 4.16% [26870 - 1484 bytes]
-// objs_dir = 0.15% [28354 - 52 bytes]
-// rooms = 1.01% [28406 - 361 bytes]
-// packdata = 0.90% [28767 - 320 bytes]
-// opcode_vrbidx_dir = 0.35% [29087 - 126 bytes]
-// opcode_vrbidx_data = 2.07% [29213 - 740 bytes]
-// opcode_pos = 1.27% [29953 - 452 bytes]
-// opcode_len = 0.63% [30405 - 226 bytes]
-// opcode_data = 11.62% [30631 - 4147 bytes]
-// roomnameid = 0.09% [34778 - 33 bytes]
-// roomdescid = 0.09% [34811 - 33 bytes]
-// roomimg = 0.09% [34844 - 33 bytes]
-// roomovrimg = 0.00% [34877 - 0 bytes]
-// objnameid = 0.48% [34877 - 170 bytes]
-// objdescid = 0.48% [35047 - 170 bytes]
-// objimg = 0.00% [35217 - 0 bytes]
-// objattr = 0.48% [35217 - 170 bytes]
-// objloc = 0.48% [35387 - 170 bytes]
-// objattrex = 0.00% [35557 - 0 bytes]
-// roomstart = 0.00% [35557 - 1 bytes]
-// roomattr = 0.09% [35558 - 33 bytes]
-// roomattrex = 0.20% [35591 - 71 bytes]
-// bitvars = 0.03% [35662 - 10 bytes]
-// vars = -99.99% [35672 - -35672 bytes]
+// shortdict = 0.85% [0 - 304 bytes]
+// advnames = 1.45% [304 - 517 bytes]
+// advdesc = 23.14% [821 - 8260 bytes]
+// msgs = 24.40% [9081 - 8710 bytes]
+// msgs2 = 23.72% [17791 - 8467 bytes]
+// verbs = 1.41% [26258 - 503 bytes]
+// objs = 4.14% [26761 - 1478 bytes]
+// objs_dir = 0.15% [28239 - 52 bytes]
+// rooms = 1.01% [28291 - 361 bytes]
+// packdata = 0.90% [28652 - 320 bytes]
+// opcode_vrbidx_dir = 0.35% [28972 - 126 bytes]
+// opcode_vrbidx_data = 2.11% [29098 - 752 bytes]
+// opcode_pos = 1.26% [29850 - 450 bytes]
+// opcode_len = 0.63% [30300 - 225 bytes]
+// opcode_data = 11.98% [30525 - 4276 bytes]
+// roomnameid = 0.09% [34801 - 33 bytes]
+// roomdescid = 0.09% [34834 - 33 bytes]
+// roomimg = 0.09% [34867 - 33 bytes]
+// roomovrimg = 0.00% [34900 - 0 bytes]
+// objnameid = 0.47% [34900 - 168 bytes]
+// objdescid = 0.47% [35068 - 168 bytes]
+// objimg = 0.00% [35236 - 0 bytes]
+// objattr = 0.47% [35236 - 168 bytes]
+// objloc = 0.47% [35404 - 168 bytes]
+// objattrex = 0.00% [35572 - 0 bytes]
+// roomstart = 0.00% [35572 - 1 bytes]
+// roomattr = 0.09% [35573 - 33 bytes]
+// roomattrex = 0.20% [35606 - 71 bytes]
+// bitvars = 0.04% [35677 - 16 bytes]
+// vars = -99.99% [35693 - -35693 bytes]
 #define room_start 0
 #define room_endgame 1
 #define room_mainsquare 2
@@ -124,114 +124,112 @@
 #define obj_market_meat 59
 #define obj_market_uniform 60
 #define obj_market_school 61
-#define obj_market_suppliers 62
+#define obj_market_supplies 62
 #define obj_market_rabbits 63
 #define obj_school_cabinets 64
 #define obj_school_stairs 65
-#define obj_school_school 66
-#define obj_school_material 67
-#define obj_schoollibrary_books 68
-#define obj_schoollibrary_floor 69
-#define obj_schoollibrary_history 70
-#define obj_schoollibrary_geography 71
-#define obj_schoollibrary_ancient 72
-#define obj_schoollibrary_botany 73
-#define obj_schoollibrary_berries 74
-#define obj_schoollibrary_geometry 75
-#define obj_schoollibrary_grimoire 76
-#define obj_schoollibrary_banishing 77
-#define obj_schoollibrary_arkylzoth 78
-#define obj_schoolalley_door 79
-#define obj_schoolalley_window 80
-#define obj_schoolalley_school 81
-#define obj_innhall_fireplace 82
-#define obj_innhall_owner 83
-#define obj_innhall_sign 84
-#define obj_innhall_inn 85
-#define obj_innhall_beer 86
-#define obj_innhall_room 87
-#define obj_innhall_kitchen 88
-#define obj_innhall_tables 89
-#define obj_upcorridor_doors 90
-#define obj_upcorridor_rooms 91
-#define obj_upcorridor_room 92
-#define obj_innroom_bed 93
-#define obj_innroom_furniture 94
-#define obj_innroom_table 95
-#define obj_innroom_drawer 96
-#define obj_innroom_mattress 97
-#define obj_kitchen_jars 98
-#define obj_kitchen_windowdoor 99
-#define obj_kitchen_kitchen 100
-#define obj_kitchen_cook 101
-#define obj_kitchen_supplies 102
-#define obj_kitchen_spices 103
-#define obj_kitchen_help 104
-#define obj_kitchen_school 105
-#define obj_kitchen_bench 106
-#define obj_southtownentrance_entrance 107
-#define obj_southtownentrance_forest 108
-#define obj_southtownentrance_paths 109
-#define obj_southtownentrance_road 110
-#define obj_southtownentrance_leg 111
-#define obj_southtownentrance_men 112
-#define obj_southtownentrance_barrels 113
-#define obj_southtownentrance_entranceguard 114
-#define obj_southtownentrance_sign 115
-#define obj_path_animals 116
-#define obj_path_paths 117
-#define obj_path_trees 118
-#define obj_path_vegetation 119
-#define obj_path_rabbits 120
-#define obj_path_plants 121
-#define obj_darkpath_branches 122
-#define obj_darkpath_paths 123
-#define obj_darkpath_trees 124
-#define obj_betweenpath_paths 125
-#define obj_mineroad_men 126
-#define obj_mineroad_barrels 127
-#define obj_mineroad_tarp 128
-#define obj_mineroad_food 129
-#define obj_mineroad_blankets 130
-#define obj_mineroad_road 131
-#define obj_camp_green 132
-#define obj_camp_red 133
-#define obj_house_door 134
-#define obj_house_house 135
-#define obj_house_walls 136
-#define obj_house_grass 137
-#define obj_houseinterior_bottle 138
-#define obj_houseinterior_fireplace 139
-#define obj_houseinterior_painting 140
-#define obj_houseinterior_windows 141
-#define obj_houseinterior_glass 142
-#define obj_houseinterior_books 143
-#define obj_ruins_slit 144
-#define obj_ruins_door 145
-#define obj_ruinsinterior_openings 146
-#define obj_ruinsinterior_blood 147
-#define obj_ruinsinterior_altar 148
-#define obj_forestpond_plants 149
-#define obj_forestpond_puddles 150
-#define obj_forestpond_stream 151
-#define obj_forestpond_watercourse 152
-#define obj_mountainpath_mountains 153
-#define obj_mountainpath_stones 154
-#define obj_fall_creature 155
-#define obj_fall_lake 156
-#define obj_fall_mountains 157
-#define obj_fall_path 158
-#define obj_fall_waterfalls 159
-#define obj_minetunnel_crossroads 160
-#define obj_minetunnel_markings 161
-#define obj_minetunnel_tunnels 162
-#define obj_forgeroom_forge 163
-#define obj_forgeroom_panel 164
-#define obj_forgeroom_cross 165
-#define obj_forgeroom_instrument 166
-#define obj_forgeroom_triangle 167
-#define obj_forgeroom_inscriptions 168
-#define obj_forgeroom_drawings 169
+#define obj_school_material 66
+#define obj_schoollibrary_books 67
+#define obj_schoollibrary_floor 68
+#define obj_schoollibrary_history 69
+#define obj_schoollibrary_geography 70
+#define obj_schoollibrary_ancient 71
+#define obj_schoollibrary_botany 72
+#define obj_schoollibrary_berries 73
+#define obj_schoollibrary_geometry 74
+#define obj_schoollibrary_grimoire 75
+#define obj_schoollibrary_banishing 76
+#define obj_schoollibrary_arkylzoth 77
+#define obj_schoolalley_door 78
+#define obj_schoolalley_window 79
+#define obj_schoolalley_school 80
+#define obj_innhall_fireplace 81
+#define obj_innhall_owner 82
+#define obj_innhall_sign 83
+#define obj_innhall_beer 84
+#define obj_innhall_room 85
+#define obj_innhall_kitchen 86
+#define obj_innhall_tables 87
+#define obj_upcorridor_doors 88
+#define obj_upcorridor_rooms 89
+#define obj_upcorridor_room 90
+#define obj_innroom_bed 91
+#define obj_innroom_furniture 92
+#define obj_innroom_table 93
+#define obj_innroom_drawer 94
+#define obj_innroom_mattress 95
+#define obj_kitchen_jars 96
+#define obj_kitchen_windowdoor 97
+#define obj_kitchen_cook 98
+#define obj_kitchen_supplies 99
+#define obj_kitchen_spices 100
+#define obj_kitchen_help 101
+#define obj_kitchen_school 102
+#define obj_kitchen_kitchen 103
+#define obj_kitchen_bench 104
+#define obj_southtownentrance_entrance 105
+#define obj_southtownentrance_forest 106
+#define obj_southtownentrance_paths 107
+#define obj_southtownentrance_road 108
+#define obj_southtownentrance_leg 109
+#define obj_southtownentrance_men 110
+#define obj_southtownentrance_barrels 111
+#define obj_southtownentrance_entranceguard 112
+#define obj_southtownentrance_sign 113
+#define obj_path_animals 114
+#define obj_path_paths 115
+#define obj_path_trees 116
+#define obj_path_vegetation 117
+#define obj_path_rabbits 118
+#define obj_path_plants 119
+#define obj_darkpath_branches 120
+#define obj_darkpath_paths 121
+#define obj_darkpath_trees 122
+#define obj_betweenpath_paths 123
+#define obj_mineroad_men 124
+#define obj_mineroad_barrels 125
+#define obj_mineroad_tarp 126
+#define obj_mineroad_food 127
+#define obj_mineroad_blankets 128
+#define obj_mineroad_road 129
+#define obj_camp_green 130
+#define obj_camp_red 131
+#define obj_house_door 132
+#define obj_house_house 133
+#define obj_house_walls 134
+#define obj_house_grass 135
+#define obj_houseinterior_bottle 136
+#define obj_houseinterior_fireplace 137
+#define obj_houseinterior_painting 138
+#define obj_houseinterior_windows 139
+#define obj_houseinterior_glass 140
+#define obj_houseinterior_books 141
+#define obj_ruins_slit 142
+#define obj_ruins_door 143
+#define obj_ruinsinterior_openings 144
+#define obj_ruinsinterior_blood 145
+#define obj_ruinsinterior_altar 146
+#define obj_forestpond_plants 147
+#define obj_forestpond_puddles 148
+#define obj_forestpond_stream 149
+#define obj_forestpond_watercourse 150
+#define obj_mountainpath_mountains 151
+#define obj_mountainpath_stones 152
+#define obj_fall_creature 153
+#define obj_fall_lake 154
+#define obj_fall_mountains 155
+#define obj_fall_path 156
+#define obj_fall_waterfalls 157
+#define obj_minetunnel_crossroads 158
+#define obj_minetunnel_markings 159
+#define obj_minetunnel_tunnels 160
+#define obj_forgeroom_forge 161
+#define obj_forgeroom_panel 162
+#define obj_forgeroom_cross 163
+#define obj_forgeroom_instrument 164
+#define obj_forgeroom_triangle 165
+#define obj_forgeroom_inscriptions 166
+#define obj_forgeroom_drawings 167
 
 #define vrb_onfirst 0
 #define vrb_onenter 1
@@ -304,78 +302,127 @@
 #define bitvar_irillisinbarrel 0
 #define bitvar_roduminbarrel 1
 #define bitvar_jacketon 2
-#define bitvar_insigniaonjacket 3
-#define bitvar_baryleaten 4
-#define bitvar_knowdarkpathroad 5
-#define bitvar_knowruinsroad 6
-#define bitvar_materialwin 7
-#define bitvar_summoningstopped 8
-#define bitvar_fulllayout 9
-#define bitvar_layoutseen 10
-#define bitvar_solvedmaze 11
-#define bitvar_knowdemonname 12
-#define bitvar_pagetaken 13
-#define bitvar_oldmandrank 14
-#define bitvar_oldmaneat 15
-#define bitvar_knowaboutcamp 16
-#define bitvar_ringgiven 17
-#define bitvar_manunderthebench 18
-#define bitvar_discoveredman 19
-#define bitvar_seeninstrument 20
-#define bitvar_midday 21
-#define bitvar_domidday 22
-#define bitvar_enteredprison 23
-#define bitvar_eryanname 24
-#define bitvar_allowedtoenter 25
-#define bitvar_womansawpendant 26
-#define bitvar_alreadysaid 27
-#define bitvar_meatneeded 28
-#define bitvar_rabbitsseen 29
-#define bitvar_knowirillis 30
-#define bitvar_rabbitgiven 31
-#define bitvar_wantjacket 32
-#define bitvar_havemoney 33
-#define bitvar_schoolseenfromnearby 34
-#define bitvar_schoolalleyseen 35
-#define bitvar_cabinetsbypassed 36
-#define bitvar_twineseen 37
-#define bitvar_cookhelps 38
-#define bitvar_pencilfound 39
-#define bitvar_mapfound 40
-#define bitvar_grimoireseen 41
-#define bitvar_knowrodum 42
-#define bitvar_knowbaryl 43
-#define bitvar_knowberriesarebaryl 44
-#define bitvar_schoolpanelremoved 45
-#define bitvar_erikisdead 46
-#define bitvar_signseen 47
-#define bitvar_erikroom 48
-#define bitvar_allowedinthekitchen 49
-#define bitvar_ownersawpendant 50
-#define bitvar_unlockedinnroom 51
-#define bitvar_cookfillbarrel 52
-#define bitvar_cookownsone 53
-#define bitvar_cooksawpendant 54
-#define bitvar_cookerik 55
-#define bitvar_eryaninprison 56
-#define bitvar_barreldutyknow 57
-#define bitvar_legseen 58
-#define bitvar_barrelsseen 59
-#define bitvar_armedmenseen 60
-#define bitvar_bowneeded 61
-#define bitvar_menasleep 62
-#define bitvar_barreltaken 63
-#define bitvar_mendead 64
-#define bitvar_carvedeyes 65
-#define bitvar_tasted 66
-#define bitvar_ruindooropened 67
-#define bitvar_eyesonaltar 68
-#define bitvar_bloodonaltar 69
-#define bitvar_defeatedfish 70
-#define bitvar_forgeon 71
-#define bitvar_gatesclosed 72
-#define bitvar_forgeseen 73
-#define bitvar_gatesseen 74
+#define bitvar_score_jacketwear 3
+#define bitvar_insigniaonjacket 4
+#define bitvar_baryleaten 5
+#define bitvar_knowdarkpathroad 6
+#define bitvar_score_knowdarkpathroad 7
+#define bitvar_knowruinsroad 8
+#define bitvar_score_knowruinsroad 9
+#define bitvar_score_insigniaonjacket 10
+#define bitvar_materialwin 11
+#define bitvar_score_materialwin 12
+#define bitvar_summoningstopped 13
+#define bitvar_fulllayout 14
+#define bitvar_layoutseen 15
+#define bitvar_score_fulllayout 16
+#define bitvar_solvedmaze 17
+#define bitvar_score_solvedmaze 18
+#define bitvar_knowdemonname 19
+#define bitvar_pagetaken 20
+#define bitvar_oldmandrank 21
+#define bitvar_oldmaneat 22
+#define bitvar_knowaboutcamp 23
+#define bitvar_ringgiven 24
+#define bitvar_manunderthebench 25
+#define bitvar_discoveredman 26
+#define bitvar_seeninstrument 27
+#define bitvar_midday 28
+#define bitvar_domidday 29
+#define bitvar_score_coinstaken 30
+#define bitvar_enteredprison 31
+#define bitvar_score_prisonentered 32
+#define bitvar_score_flaskfilled 33
+#define bitvar_eryanname 34
+#define bitvar_score_allowedtocell 35
+#define bitvar_allowedtoenter 36
+#define bitvar_womansawpendant 37
+#define bitvar_alreadysaid 38
+#define bitvar_score_geometrysection 39
+#define bitvar_meatneeded 40
+#define bitvar_rabbitsseen 41
+#define bitvar_knowirillis 42
+#define bitvar_rabbitgiven 43
+#define bitvar_score_breadtaken 44
+#define bitvar_wantjacket 45
+#define bitvar_havemoney 46
+#define bitvar_schoolseenfromnearby 47
+#define bitvar_schoolalleyseen 48
+#define bitvar_cabinetsbypassed 49
+#define bitvar_twineseen 50
+#define bitvar_score_twineseen 51
+#define bitvar_cookhelps 52
+#define bitvar_score_cabinetsbypassed 53
+#define bitvar_pencilfound 54
+#define bitvar_mapfound 55
+#define bitvar_score_mapfound 56
+#define bitvar_grimoireseen 57
+#define bitvar_score_grimoireseen 58
+#define bitvar_knowrodum 59
+#define bitvar_knowbaryl 60
+#define bitvar_knowberriesarebaryl 61
+#define bitvar_score_berriesrecognized 62
+#define bitvar_score_documentstaken 63
+#define bitvar_score_spellseen 64
+#define bitvar_pageshown 65
+#define bitvar_score_spelltaken 66
+#define bitvar_schoolpanelremoved 67
+#define bitvar_score_schoolpanelremoved 68
+#define bitvar_erikisdead 69
+#define bitvar_signseen 70
+#define bitvar_erikroom 71
+#define bitvar_allowedinthekitchen 72
+#define bitvar_ownersawpendant 73
+#define bitvar_score_roomkey 74
+#define bitvar_unlockedinnroom 75
+#define bitvar_score_flask 76
+#define bitvar_score_eriknotebook 77
+#define bitvar_cookfillbarrel 78
+#define bitvar_cookownsone 79
+#define bitvar_cooksawpendant 80
+#define bitvar_score_cookerik 81
+#define bitvar_cookerik 82
+#define bitvar_eryaninprison 83
+#define bitvar_score_irilliswanted 84
+#define bitvar_score_cookhelps 85
+#define bitvar_barreldutyknow 86
+#define bitvar_legseen 87
+#define bitvar_barrelsseen 88
+#define bitvar_score_barreldutyknow 89
+#define bitvar_armedmenseen 90
+#define bitvar_score_insigniataken 91
+#define bitvar_score_rabbittaken 92
+#define bitvar_bowneeded 93
+#define bitvar_score_irillistaken 94
+#define bitvar_score_rodumtaken 95
+#define bitvar_bowmade 96
+#define bitvar_score_bowmade 97
+#define bitvar_menasleep 98
+#define bitvar_barreltaken 99
+#define bitvar_score_barreltaken 100
+#define bitvar_mendead 101
+#define bitvar_score_menasleep 102
+#define bitvar_score_menasleeptried 103
+#define bitvar_score_eryanname 104
+#define bitvar_carvedeyes 105
+#define bitvar_score_carvedeyes 106
+#define bitvar_score_barylfound 107
+#define bitvar_tasted 108
+#define bitvar_score_tasted 109
+#define bitvar_score_knowdemonname 110
+#define bitvar_ruindooropened 111
+#define bitvar_score_ruindooropened 112
+#define bitvar_eyesonaltar 113
+#define bitvar_bloodonaltar 114
+#define bitvar_score_summoningstopped 115
+#define bitvar_score_fishtaken 116
+#define bitvar_defeatedfish 117
+#define bitvar_score_defeatedfish 118
+#define bitvar_forgeon 119
+#define bitvar_gatesclosed 120
+#define bitvar_forgeseen 121
+#define bitvar_gatesseen 122
+#define bitvar_score_gatesclosed 123
 
 #define attr_visible 1
 #define attr_actor 2
